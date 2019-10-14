d55946468a1fed7f1ac72debf4b8442df326ef48
Markus Anders <m_anders14@cs.uni-kl.de>
2019-10-14 16:25:20 +0200

    Changes

---
+++ utility.cpp
@@ -0,0 +1,5 @@
+//
+// Created by markus on 14.10.19.
+//
+
+#include "utility.h"
---
+++ utility.h
@@ -0,0 +1,14 @@
+//
+// Created by markus on 14.10.19.
+//
+
+#ifndef DEJAVU_UTILITY_H
+#define DEJAVU_UTILITY_H
+
+
+class utility {
+
+};
+
+
+#endif //DEJAVU_UTILITY_H
