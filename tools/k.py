ef3b6d4a0ce712d5ab12fea380ecea0417fe069c
Markus <m_anders14@cs.uni-kl.de>
2023-04-12 10:45:19 +0200

    more cleanup

--- tools/k.py
+++
@@ -1,8 +0,0 @@
-n = 2500
-f = open("k%d.dimacs" %n,"w+")
-
-f.write("p edge {} {}\n".format(n, int((n * (n - 1)) / 2)))
-
-for i in range(1, n + 1):
-    for j in range(i + 1, n + 1):
-     f.write("e {} {}\n".format(i, j))
